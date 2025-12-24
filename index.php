a05bdd8612c262dca5708e2cdfa0057baddc1945
Kairi <imamazubair@gmail.com>
2025-12-24 20:53:19 +0500

    random

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ session_start();
     <?php endif; ?>
 
     <footer>
-        <p id="welcome-paragraph">Welcome to <b id="msg-spritee">SpRITEE</b>! Probably the most fun and intriguing websites out there. What's it about? Welllll, It's just a little pixel-art maker! A place where one can draw ANYTHING pixelated. I tried to keep it as interactive as possible. Hope you enjoy!!!! :3</p>
+        <p id="welcome-paragraph">Welcome to <b id="msg-spritee">SpRITEE</b>! Probably the most fun and intriguing websites out there. What's it about? Welllll, It's just a little pixel-art maker! A place where one can draw ANYTHING pixelated. We tried to keep it as interactive as possible. Hope you enjoy! :3</p>
         </p>
         <p id="copyright">© 2025 Spritee. All rights reserved.</p>
     </footer>
