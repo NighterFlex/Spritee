3a21528feeba329dfb7ea4ca0aa56f817c7e3237
Kairi <imamazubair@gmail.com>
2025-12-29 17:36:07 +0500

    fixed the database

--- spritee.sql
+++ spritee.sql
@@ -1,21 +1,24 @@
-CREATE DATABASE spritee_db;
-use spritee_db;
-
 CREATE TABLE registration (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(20) NOT NULL,
-    country VARCHAR(50)
+    password VARCHAR(100) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    gender ENUM('male', 'female') NOT NULL
 );
 
-
-
-CREATE TABLE gallery(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE gallery (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     art_name VARCHAR(100) DEFAULT 'UNTITLED',
-    image_data MEDIUMTEXT NOT NULL,   #base64
-    CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES registration(id) ON DELETE CASCADE
+    image_data MEDIUMTEXT NOT NULL,  -- base64 image
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES registration(id)
+        ON DELETE CASCADE
 );
+
+
+
+--796f75726d6f6d
